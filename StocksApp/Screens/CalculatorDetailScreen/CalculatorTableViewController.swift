91d0e6d9364814fbde61bb799a1d22eeff83be9c
borisbolsh <bolsh.bo@yandex.ru>
2022-04-05 23:37:12 +0300

    Add auto height cell, fix some detail info in cell

--- StocksApp/Screens/CalculatorDetailScreen/CalculatorTableViewController.swift
+++ StocksApp/Screens/CalculatorDetailScreen/CalculatorTableViewController.swift
@@ -4,22 +4,20 @@ final class CalculatorTableViewController: UITableViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UINib(nibName: String(describing: CalculatorTableViewCell.self), bundle: nil), forCellReuseIdentifier: String(describing: CalculatorTableViewCell.self))
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 100
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: CalculatorTableViewCell.self), for: indexPath) as! CalculatorTableViewCell
+
         return cell
     }
 }
